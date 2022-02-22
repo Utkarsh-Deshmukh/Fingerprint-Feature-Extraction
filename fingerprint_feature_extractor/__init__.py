64c592f21c4e64ed915c6118444a9c59371cf50b
Utkarsh <utkarsh.deshmukh@gmail.com>
2022-02-21 23:28:57 -0800

    bug fix

--- fingerprint_feature_extractor/__init__.py
+++ fingerprint_feature_extractor/__init__.py
@@ -7,10 +7,10 @@ import math
 
 class MinutiaeFeature(object):
     def __init__(self, locX, locY, Orientation, Type):
-        self.locX = locX;
-        self.locY = locY;
-        self.Orientation = Orientation;
-        self.Type = Type;
+        self.locX = locX
+        self.locY = locY
+        self.Orientation = Orientation
+        self.Type = Type
 
 class FingerprintFeatureExtractor(object):
     def __init__(self):
@@ -27,10 +27,10 @@ class FingerprintFeatureExtractor(object):
 
     def __computeAngle(self, block, minutiaeType):
         angle = []
-        (blkRows, blkCols) = np.shape(block);
+        (blkRows, blkCols) = np.shape(block)
         CenterX, CenterY = (blkRows - 1) / 2, (blkCols - 1) / 2
         if (minutiaeType.lower() == 'termination'):
-            sumVal = 0;
+            sumVal = 0
             for i in range(blkRows):
                 for j in range(blkCols):
                     if ((i == 0 or i == blkRows - 1 or j == 0 or j == blkCols - 1) and block[i][j] != 0):
@@ -41,10 +41,10 @@ class FingerprintFeatureExtractor(object):
             return (angle)
 
         elif (minutiaeType.lower() == 'bifurcation'):
-            (blkRows, blkCols) = np.shape(block);
+            (blkRows, blkCols) = np.shape(block)
             CenterX, CenterY = (blkRows - 1) / 2, (blkCols - 1) / 2
             angle = []
-            sumVal = 0;
+            sumVal = 0
             for i in range(blkRows):
                 for j in range(blkCols):
                     if ((i == 0 or i == blkRows - 1 or j == 0 or j == blkCols - 1) and block[i][j] != 0):
@@ -55,36 +55,36 @@ class FingerprintFeatureExtractor(object):
             return (angle)
 
     def __getTerminationBifurcation(self):
-        self._skel = self._skel == 255;
-        (rows, cols) = self._skel.shape;
-        self.minutiaeTerm = np.zeros(self._skel.shape);
-        self.minutiaeBif = np.zeros(self._skel.shape);
+        self._skel = self._skel == 255
+        (rows, cols) = self._skel.shape
+        self.minutiaeTerm = np.zeros(self._skel.shape)
+        self.minutiaeBif = np.zeros(self._skel.shape)
 
         for i in range(1, rows - 1):
             for j in range(1, cols - 1):
                 if (self._skel[i][j] == 1):
-                    block = self._skel[i - 1:i + 2, j - 1:j + 2];
-                    block_val = np.sum(block);
+                    block = self._skel[i - 1:i + 2, j - 1:j + 2]
+                    block_val = np.sum(block)
                     if (block_val == 2):
-                        self.minutiaeTerm[i, j] = 1;
+                        self.minutiaeTerm[i, j] = 1
                     elif (block_val == 4):
-                        self.minutiaeBif[i, j] = 1;
+                        self.minutiaeBif[i, j] = 1
 
         self._mask = convex_hull_image(self._mask > 0)
         self._mask = erosion(self._mask, square(5))  # Structuing element for mask erosion = square(5)
         self.minutiaeTerm = np.uint8(self._mask) * self.minutiaeTerm
 
     def __removeSpuriousMinutiae(self, minutiaeList, img, thresh):
-        img = img * 0;
-        SpuriousMin = [];
-        numPoints = len(minutiaeList);
+        img = img * 0
+        SpuriousMin = []
+        numPoints = len(minutiaeList)
         D = np.zeros((numPoints, numPoints))
         for i in range(1,numPoints):
             for j in range(0, i):
                 (X1,Y1) = minutiaeList[i]['centroid']
                 (X2,Y2) = minutiaeList[j]['centroid']
 
-                dist = np.sqrt((X2-X1)**2 + (Y2-Y1)**2);
+                dist = np.sqrt((X2-X1)**2 + (Y2-Y1)**2)
                 D[i][j] = dist
                 if(dist < thresh):
                     SpuriousMin.append(i)
@@ -93,20 +93,20 @@ class FingerprintFeatureExtractor(object):
         SpuriousMin = np.unique(SpuriousMin)
         for i in range(0,numPoints):
             if(not i in SpuriousMin):
-                (X,Y) = np.int16(minutiaeList[i]['centroid']);
-                img[X,Y] = 1;
+                (X,Y) = np.int16(minutiaeList[i]['centroid'])
+                img[X,Y] = 1
 
-        img = np.uint8(img);
+        img = np.uint8(img)
         return(img)
 
     def __cleanMinutiae(self, img):
-        self.minutiaeTerm = skimage.measure.label(self.minutiaeTerm, connectivity=2);
+        self.minutiaeTerm = skimage.measure.label(self.minutiaeTerm, connectivity=2)
         RP = skimage.measure.regionprops(self.minutiaeTerm)
-        self.minutiaeTerm = self.__removeSpuriousMinutiae(RP, np.uint8(img), 10);
+        self.minutiaeTerm = self.__removeSpuriousMinutiae(RP, np.uint8(img), 10)
 
     def __performFeatureExtraction(self):
         FeaturesTerm = []
-        self.minutiaeTerm = skimage.measure.label(self.minutiaeTerm, connectivity=2);
+        self.minutiaeTerm = skimage.measure.label(self.minutiaeTerm, connectivity=2)
         RP = skimage.measure.regionprops(np.uint8(self.minutiaeTerm))
 
         WindowSize = 2  # --> For Termination, the block size must can be 3x3, or 5x5. Hence the window selected is 1 or 2
@@ -119,7 +119,7 @@ class FingerprintFeatureExtractor(object):
                 FeaturesTerm.append(MinutiaeFeature(row, col, angle, 'Termination'))
 
         FeaturesBif = []
-        self.minutiaeBif = skimage.measure.label(self.minutiaeBif, connectivity=2);
+        self.minutiaeBif = skimage.measure.label(self.minutiaeBif, connectivity=2)
         RP = skimage.measure.regionprops(np.uint8(self.minutiaeBif))
         WindowSize = 1  # --> For Bifurcation, the block size must be 3x3. Hence the window selected is 1
         for i in RP:
@@ -140,34 +140,25 @@ class FingerprintFeatureExtractor(object):
         FeaturesTerm, FeaturesBif = self.__performFeatureExtraction()
         return(FeaturesTerm, FeaturesBif)
 
-    def showResults(self):
-        BifLabel = skimage.measure.label(self.minutiaeBif, connectivity=2);
-        TermLabel = skimage.measure.label(self.minutiaeTerm, connectivity=2);
-
-        minutiaeBif = TermLabel * 0;
-        minutiaeTerm = BifLabel * 0;
-
+    def showResults(self, FeaturesTerm, FeaturesBif):
+        
         (rows, cols) = self._skel.shape
         DispImg = np.zeros((rows, cols, 3), np.uint8)
-        DispImg[:, :, 0] = 255*self._skel;
-        DispImg[:, :, 1] = 255*self._skel;
-        DispImg[:, :, 2] = 255*self._skel;
+        DispImg[:, :, 0] = 255*self._skel
+        DispImg[:, :, 1] = 255*self._skel
+        DispImg[:, :, 2] = 255*self._skel
 
-        RP = skimage.measure.regionprops(BifLabel)
-        for idx, i in enumerate(RP):
-            (row, col) = np.int16(np.round(i['Centroid']))
-            minutiaeBif[row, col] = 1;
-            (rr, cc) = skimage.draw.circle_perimeter(row, col, 3);
-            skimage.draw.set_color(DispImg, (rr, cc), (255, 0, 0));
+        for idx, curr_minutiae in enumerate(FeaturesTerm):
+            row, col = curr_minutiae.locX, curr_minutiae.locY
+            (rr, cc) = skimage.draw.circle_perimeter(row, col, 3)
+            skimage.draw.set_color(DispImg, (rr, cc), (0, 0, 255))
 
-        RP = skimage.measure.regionprops(TermLabel)
-        for idx, i in enumerate(RP):
-            (row, col) = np.int16(np.round(i['Centroid']))
-            minutiaeTerm[row, col] = 1;
-            (rr, cc) = skimage.draw.circle_perimeter(row, col, 3);
-            skimage.draw.set_color(DispImg, (rr, cc), (0, 0, 255));
-
-        cv2.imshow('a', DispImg);
+        for idx, curr_minutiae in enumerate(FeaturesBif):
+            row, col = curr_minutiae.locX, curr_minutiae.locY
+            (rr, cc) = skimage.draw.circle_perimeter(row, col, 3)
+            skimage.draw.set_color(DispImg, (rr, cc), (255, 0, 0))
+        
+        cv2.imshow('output', DispImg)
         cv2.waitKey(0)
 
 
@@ -176,6 +167,6 @@ def extract_minutiae_features(img, showResult=False):
     FeaturesTerm, FeaturesBif = feature_extractor.extractMinutiaeFeatures(img)
 
     if(showResult):
-        feature_extractor.showResults()
+        feature_extractor.showResults(FeaturesTerm, FeaturesBif)
 
     return(FeaturesTerm, FeaturesBif)
